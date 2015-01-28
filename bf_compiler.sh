96a37bda94aa658105138ef726af3f3720f374f7
James Riley Wynne III <wynnejr@ornl.gov>
2015-01-28 11:53:16 -0500

    Added compatability to BF++

--- bf_compiler.sh
+++ bf_compiler.sh
@@ -4,11 +4,48 @@ main() {
   INPUT="${1}"
   OUTPUT="${2}"
   TEMP="${INPUT}.c"
-
+  FILE=' 
+  begFileName = ptr;
+  if(!isOpen) {
+    int* cur = ptr
+    int idx = 0;
+    while(cur) {
+      name[idx] = cur;
+      ++ptr;
+      cur = *ptr;
+      idx++;
+    }
+    if(f = fopen(name,w+b)) {
+      ptr = begFileName;
+      ptr* = 0x00;
+    }
+    else {
+      ptr = begFileName;
+      ptr* = 0xFF;
+    }
+  } else {
+    fclose(f);
+  }'
+  READBYTE='
+  if(!(*ptr = fread(&curByte,sizeOf(char),1,f)))
+    *ptr = 0x00;
+  '
+  WRITEBYTE='
+  if(!(*ptr = fwrite(ptr, sizeOf(char),1,f)))
+    *ptr = 0x00;
+  '
   BEGIN='#include <stdio.h>
+  bool isOpen = false;
+  #include <fstream>
   int main(){
   int array[100000] = {0};
-  int *ptr=array;'
+  int *ptr=array;
+  int cur = *ptr;
+  char* fptr;
+  char* name;
+  int* begFileName;
+  char curByte = 0;
+  FILE* f;'
   echo "$BEGIN" > "${TEMP}"
   while read -n1 char; do
     case $char in
@@ -28,11 +65,17 @@ main() {
         echo -n 'while (*ptr) {' >> "${TEMP}";;
       \])
         echo -n '}' >> "${TEMP}";;
+      \#)
+        echo -n "$FILE" >> "${TEMP}";;
+      \:)
+        echo -n "$READBYTE" >> "${TEMP}";;
+      \;) 
+        echo -n "$WRITEBYTE" >> "${TEMP}";;
     esac
   done < "${INPUT}"
   echo >> "${TEMP}"
   echo '}' >> "${TEMP}"
-  gcc -o "${OUTPUT}" "${TEMP}"
+  g++ -o "${OUTPUT}" "${TEMP}"
   rm "${TEMP}"
 }
 main "${@}"
